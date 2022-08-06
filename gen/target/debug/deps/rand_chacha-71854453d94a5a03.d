34e4959aa75ffed9259b4d1eaed102d9ae57ee93
Mathias Berger--fruhinsholz <throw_new_Error@protonmail.com>
2022-08-06 23:29:09 +0200

    update gitignore

--- gen/target/debug/deps/rand_chacha-71854453d94a5a03.d
+++
@@ -1,9 +0,0 @@
-/Users/mberger-/Documents/lem-in/gen/target/debug/deps/rand_chacha-71854453d94a5a03.rmeta: /Users/mberger-/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /Users/mberger-/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /Users/mberger-/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/Users/mberger-/Documents/lem-in/gen/target/debug/deps/librand_chacha-71854453d94a5a03.rlib: /Users/mberger-/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /Users/mberger-/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /Users/mberger-/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/Users/mberger-/Documents/lem-in/gen/target/debug/deps/rand_chacha-71854453d94a5a03.d: /Users/mberger-/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /Users/mberger-/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /Users/mberger-/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/Users/mberger-/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
-/Users/mberger-/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
-/Users/mberger-/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
--- gen/target/debug/lem-in-gen.d
+++
@@ -1,1 +0,0 @@
-/Users/mberger-/Documents/lem-in/gen/target/debug/lem-in-gen: /Users/mberger-/Documents/lem-in/gen/src/main.rs
